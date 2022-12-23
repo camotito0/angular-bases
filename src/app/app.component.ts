54bcf6b66ee34e11b4383c1c80112d5cf6c3b55d
camotito0 <cecilianallerena@gmail.com>
2022-12-23 11:19:02 -0500

    Adding method acumular in app component ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // name of the component
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  title : string = 'Count app';
+  numero: number = 10;
+  base  :number = 5;
+
+  acumular(value:number) {
+    this.numero +=value
+  }
 }
