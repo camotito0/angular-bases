e157489d850f1832b3786c6b29459167f9f4c2eb
camotito0 <cecilianallerena@gmail.com>
2022-12-27 12:00:22 -0500

    Modularizing counter and hero component, adding ngif else directive in listado component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heros/hero/hero.component';
+import { CounterModule } from './counter/counter.module';
+import { HerosModule } from './heros/heros.module';
 
 @NgModule({
   declarations: [ // se declaran componentes
     AppComponent,
-    CounterComponent,
-    HeroComponent
   ],
   imports: [
-    BrowserModule // se importan modulos
+    BrowserModule, // se importan modulos
+    HerosModule,
+    CounterModule
   ],
   providers: [], // servicios de modulos
   bootstrap: [AppComponent] // app principal
